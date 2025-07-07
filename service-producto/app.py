ffaea8457ac28a41faf549e65944309d42f8af44
JunalChowdhuryG <chowdhurygomezjunaljohir@gmail.com>
2025-07-07 14:10:11 -0500

    feat(servicio-producto): agregar servicio producto

---
+++ service-producto/app.py
@@ -0,0 +1,43 @@
+
+
+
+
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+from typing import Dict
+
+app = FastAPI(title="Servicio Producto")
+
+productos: Dict[int, Dict] = {}
+
+#clase producto
+class Producto(BaseModel):
+    id: int      
+    name: str    
+    descripcion: str 
+
+
+@app.post("/productos/", response_model=Producto)
+def crear_producto(producto: Producto):
+    if producto.id in productos:
+        raise HTTPException(status_code=400, detail="producto existe")
+    productos[producto.id] = producto.dict()
+    return producto
+
+@app.get("/productos/{id}", response_model=Producto)
+def buscar_producto(id: int):
+    if id not in productos:
+        raise HTTPException(status_code=404, detail="producto no encontrado")
+    return productos[id]
+
+@app.delete("/productos/{id}")
+def eliminar_producto(id: int):
+    if id not in productos:
+        raise HTTPException(status_code=404, detail="producto no encontrado")
+    del productos[id]
+    return {"detalle": "producto eliminado"}
+
+
+@app.get("/health")
+def health():
+    return {"status": "ok"}
---
+++ service-producto/entrypoint.sh
@@ -0,0 +1,7 @@
+echo "Iniciando el contenedor del servicio de producto"
+
+echo "Instalando dependencias"
+
+echo "Ejecutando el servidor Uvicorn"
+
+echo "servicio de usuario iniciado en el localhost:8000"
