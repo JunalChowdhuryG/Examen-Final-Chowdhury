85c5b8c0589f92cf066ae3022438623d1dff97ad
JunalChowdhuryG <chowdhurygomezjunaljohir@gmail.com>
2025-07-07 14:10:51 -0500

    feat(servicio-usuario): agregar servicio usuario

---
+++ service-usuario/entrypoint.sh
@@ -0,0 +1,7 @@
+echo "Iniciando el contenedor del servicio de usuario"
+
+echo "Instalando dependencias"
+
+echo "Ejecutando el servidor Uvicorn"
+
+echo "servicio de usuario iniciado en el localhost:8001"
